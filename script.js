c8fb8f749176c7dffd7daaac2a1da0963eefe47d
devramirez <aramirezdev1@gmail.com>
2023-05-18 17:13:40 -0400

    created a couple functions and dayjs to get data

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+$document().ready(function () { // render html & css to load first
+    var todayDate = day.js().format("dddd, MMMM D, YYYY, H:mma") // using day.js to retrieve time data
+    $("#currentDay").text(todayDate);
+
+    function timeTracker()
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
